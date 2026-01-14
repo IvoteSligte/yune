be9780a20aaf174843c61a3fc68aa50021a49b9a
Ivo te Sligte <ivotesligte@gmail.com>
2026-01-14 10:47:15 +0100

    moved cpp running code to cpp/

---
+++ cpp/run.go
@@ -0,0 +1,80 @@
+package cpp
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+)
+
+func format(code string) (formatted string, err error) {
+	cmd := exec.Command("clang-format")
+	if cmd.Err != nil {
+		err = cmd.Err
+		return
+	}
+	cmd.Stdin = strings.NewReader(code)
+	outputBytes, err := cmd.Output()
+	if err != nil {
+		return
+	}
+	formatted = string(outputBytes)
+	return
+}
+
+func PrintFormatted(code string) {
+	formatted, err := format(code)
+	if err != nil {
+		log.Println("Error formatting C++ with clang-format:", err)
+		log.Println("Unformatted C++:")
+		fmt.Println(code)
+	} else {
+		fmt.Println(formatted)
+	}
+}
+
+func createFile(dir string, name string) *os.File {
+	file, err := os.Create(path.Join(dir, name))
+	if err != nil {
+		log.Fatalln("Failed to create file during compilation process. Error:", err)
+	}
+	return file
+}
+
+func writeFile(dir string, name string, contents string) *os.File {
+	file := createFile(dir, name)
+	_, err := file.WriteString(contents)
+	if err != nil {
+		log.Fatalln("Failed to write to file during compilation process. Error:", err)
+	}
+	return file
+}
+
+func Run(module Module) {
+	dir, err := os.MkdirTemp("", "yune-build")
+	if err != nil {
+		log.Fatalln("Failed to create temporary directory during compilation process. Error:", err)
+	}
+	defer os.RemoveAll(dir)
+
+	header := module.GenHeader()
+	implementation := module.String()
+	writeFile(dir, "code.hpp", header)
+	writeFile(dir, "code.cpp", "#include \"code.hpp\"\n"+implementation)
+	implementationPath := path.Join(dir, "code.cpp")
+	binaryPath := path.Join(dir, "code.bin")
+
+	cmd := exec.Command("clang", []string{implementationPath, "-o", binaryPath}...)
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalln("Failed to compile code. Error:", err)
+	}
+	err = exec.Command(binaryPath).Run()
+	if err != nil {
+		log.Fatalln("Failed to run code. Error:", err)
+	}
+	// NOTE: main function is assumed to exist
+}
--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"path"
 	"strconv"
-	"strings"
 	"yune/cpp"
 	"yune/parser"
 
@@ -63,76 +60,6 @@ func printTokens(lexer antlr.Recognizer, tokenStream *antlr.CommonTokenStream) {
 	}
 }
 
-func formatCpp(code string) (formatted string, err error) {
-	cmd := exec.Command("clang-format")
-	if cmd.Err != nil {
-		err = cmd.Err
-		return
-	}
-	cmd.Stdin = strings.NewReader(code)
-	outputBytes, err := cmd.Output()
-	if err != nil {
-		return
-	}
-	formatted = string(outputBytes)
-	return
-}
-
-func printCpp(code string) {
-	formatted, err := formatCpp(code)
-	if err != nil {
-		log.Println("Error formatting C++ with clang-format:", err)
-		log.Println("Unformatted C++:")
-		fmt.Println(code)
-	} else {
-		fmt.Println(formatted)
-	}
-}
-
-func createFile(dir string, name string) *os.File {
-	file, err := os.Create(path.Join(dir, name))
-	if err != nil {
-		log.Fatalln("Failed to create file during compilation process. Error:", err)
-	}
-	return file
-}
-
-func writeTempFile(dir string, name string, contents string) *os.File {
-	file := createFile(dir, name)
-	_, err := file.WriteString(contents)
-	if err != nil {
-		log.Fatalln("Failed to write to file during compilation process. Error:", err)
-	}
-	return file
-}
-
-func runCppModule(module cpp.Module) {
-	dir, err := os.MkdirTemp("", "yune-build")
-	if err != nil {
-		log.Fatalln("Failed to create temporary directory during compilation process. Error:", err)
-	}
-	defer os.RemoveAll(dir)
-
-	header := module.GenHeader()
-	implementation := module.String()
-	writeTempFile(dir, "code.hpp", header)
-	writeTempFile(dir, "code.cpp", "#include \"code.hpp\"\n"+implementation)
-	implementationPath := path.Join(dir, "code.cpp")
-	binaryPath := path.Join(dir, "code.bin")
-
-	cmd := exec.Command("clang", []string{implementationPath, "-o", binaryPath}...)
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		log.Fatalln("Failed to compile code. Error:", err)
-	}
-	err = exec.Command(binaryPath).Run()
-	if err != nil {
-		log.Fatalln("Failed to run code. Error:", err)
-	}
-	// NOTE: main function is assumed to exist
-}
-
 func main() {
 	data := readFile()
 	println(data)
@@ -161,9 +88,9 @@ func main() {
 		log.Fatalln("Errors found, exiting.")
 	}
 	fmt.Println("--- CPP header ---")
-	printCpp(cppModule.GenHeader())
+	cpp.PrintFormatted(cppModule.GenHeader())
 	fmt.Println("--- CPP implementation (should include header) ---")
-	printCpp(cppModule.String())
+	cpp.PrintFormatted(cppModule.String())
 	fmt.Println("--- Output ---")
-	runCppModule(cppModule)
+	cpp.Run(cppModule)
 }
