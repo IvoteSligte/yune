8622f0f502fbc7e699afdce7efb0043fc408e6f8
Ivo te Sligte <ivotesligte@gmail.com>
2026-04-08 09:53:44 +0200

    rm static linking of shared object

--- cpp/run.go
+++ cpp/run.go
@@ -67,8 +67,8 @@ func CompileLibrary(module Module) {
 
 	fmt.Println("-- Clang++ log --")
 	includes := os.ExpandEnv("-I$PWD/cpp")
-	// Creates a C++23 shared library that includes all its dependencies.
-	cmd := exec.Command("clang++", "-O1", "-shared", "-static", "-fPIC", "-std=c++23", implementationPath, "-o", libraryPath, includes)
+	// Creates a C++23 shared library
+	cmd := exec.Command("clang++", "-O1", "-shared", "-fPIC", "-std=c++23", implementationPath, "-o", libraryPath, includes)
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
